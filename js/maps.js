7ae7670999f1c7cc176155b2ef867e27f4512bf8
Nate Browne <natebrowne@outlook.com>
2019-03-24 23:20:49 -0700

    Out with the old, in with the new. Full redesign.

---
+++ js/maps.js
@@ -0,0 +1,27 @@
+'use strict';
+
+function gen_map() {
+
+  // Generate a new map based on UCSD CSE building coordinates
+  let latLong = new google.maps.LatLng(32.8818, -117.2335);
+
+  // Create variable of map options to use
+  let mapOptions = {
+    center: latLong, // setting the centerpoint
+    zoom: 12, // default zoom level
+    scrollwheel: false, // make map zoomable
+    draggable: true, // make map interactive
+    mapTypeId: google.maps.MapTypeId.ROADMAP // set the type of the map
+  };
+
+  // Create the map
+  let map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
+
+  // Add a location marker to the map
+  let marker = new google.maps.Marker({
+    position: mapOptions.center,
+  });
+
+  // Put the marker on the map
+  marker.setMap(map);
+}
