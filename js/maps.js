2c8e0383cabe067949d5027808651d65643f07bc
Nate Browne <natebrowne@outlook.com>
2019-03-25 17:22:41 -0700

    Added Google analytics

--- js/maps.js
+++ js/maps.js
@@ -3,12 +3,13 @@
 function gen_map() {
 
   // Generate a new map based on UCSD CSE building coordinates
+  // SDSU CS coordinates: 32.776742, -117.069266
   let latLong = new google.maps.LatLng(32.8818, -117.2335);
 
   // Create variable of map options to use
   let mapOptions = {
     center: latLong, // setting the centerpoint
-    zoom: 12, // default zoom level
+    zoom: 15, // default zoom level
     scrollwheel: false, // make map zoomable
     draggable: true, // make map interactive
     mapTypeId: google.maps.MapTypeId.ROADMAP // set the type of the map
