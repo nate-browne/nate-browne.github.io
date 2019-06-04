74f0695f771957d75e319174766d6e5f658f5c6a
Nate Browne <npcompletenate@protonmail.com>
2019-06-03 22:49:02 -0700

    updated resume, added some nice things

--- js/maps.js
+++ js/maps.js
@@ -2,10 +2,24 @@
 
 function gen_map() {
 
-  // Generate a new map based on UCSD CSE building coordinates
-  // SDSU CS coordinates: 32.776742, -117.069266
+  // Coordinates of CSE buildings
+  let coordinates = {
+    'Duke': '36.004600 -78.83729',
+    'Hawaii': '21.297760 -157.815140',
+    'USC': '34.019910 -118.288980',
+    'UCRiverside': '33.9752643 -117.3260679',
+    'UCLA': '34.069530 -118.444870',
+    'UDub': '47.656420 -122.304780',
+    'Virginia': '38.031979 -78.511192',
+    'Princeton': '40.350720 -74.652070',
+    'NYU': '40.728680 -73.995580',
+    'UMD': '38.999610 -76.942200',
+    'Columbia': '40.809520 -73.959530',
+    'UCSD': '32.8818 -117.2335'
+  }
+
   let mapOptions = {
-    center: new google.maps.LatLng(32.8818, -117.2335),
+    center: new google.maps.LatLng(coordinates.UCSD.split(" ")[0], coordinates.UCSD.split(" ")[1]),
     zoom: 15,
     scrollwheel: false, // make map zoomable
     draggable: true, // make map interactive
